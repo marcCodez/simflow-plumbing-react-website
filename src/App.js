b8fb01089995da6e283035f0f7d0fed32d781921
Marc Pulumbarit <lastcodebender@Marcs-MacBook-Air.local>
2021-03-06 02:17:32 +1100

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+import Navbar from './components/Navbar';
 import './Main.scss';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-    hi
+    <div>
+      <Router>
+        <Navbar/>
+        <Switch>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+function Navbar() {
+    return (
+                <nav id="nav-bar">
+                     <img src="/images/logo.png" className="logo" />
+                        <ul className="nav-menu">
+                            <li><NavLink 
+                            exact to="/"
+                            className="nav-links"
+                             >Home</NavLink></li>
+                            <li><NavLink exact to="about" className="nav-links">About</NavLink></li>
+                            <li><NavLink exact to="services" className="nav-links" >Services</NavLink></li>
+                            <li><NavLink exact to="/promotions" className="nav-links" >Promotions</NavLink></li>
+                            <li><NavLink exact to="/contact" className="nav-links">Contact</NavLink></li>
+                        </ul>
+                </nav>
+    )
+}
+
+export default Navbar
