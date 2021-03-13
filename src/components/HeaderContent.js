eb91b3ad30c28bd0f394fd339ac61545a222422a
Marc Pulumbarit <lastcodebender@Marcs-MacBook-Air.local>
2021-03-13 16:32:42 +1100

    header overlay and resize

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -3,32 +3,32 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function HeaderContent() {
     return (
-        <div>
-            <Carousel fade="true" interval="3000">
-                <Carousel.Item>
+        <header>
+            <Carousel id="hero"  interval="3000">
+                <Carousel.Item style={{'height':"800px"}}>
                     <img className="d-block w-100" src="/images/work/p1.jpg" 
-                    alt="Image one" style={{height: "100vh"}}
+                    alt="Image one" style={{height: "100%"}}
                     />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item style={{'height':"900px"}}>
                     <img className="d-block w-100" src="/images/work/p2.jpg" 
-                    alt="Image one" style={{height: "100vh"}}
+                    alt="Image one" style={{height: "100%"}}
                     />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item style={{'height':"900px"}}>
                     <img className="d-block w-100" src="/images/work/p3.jpg" 
-                    alt="Image two" style={{height: "100vh"}}
+                    alt="Image two" style={{height: "100%"}}
                     />
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item style={{'height':"900px"}}>
                     <img className="d-block w-100" src="/images/work/p4.jpg" 
-                    alt="Image three" style={{height: "100vh"}}
+                    alt="Image three" style={{height: "100%"}}
                     />
                 </Carousel.Item>
 
             </Carousel>
             
-        </div>
+        </header>
     )
 }
 
