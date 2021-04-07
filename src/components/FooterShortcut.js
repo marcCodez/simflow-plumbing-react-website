bfdb58835870d5cce6a97994ccc8482951a58b9d
Marc Pulumbarit <lastcodebender@Marcs-MacBook-Air.local>
2021-04-07 16:01:08 +1000

    fix alignments and color

--- src/components/FooterShortcut.js
+++ src/components/FooterShortcut.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 function FooterShortcut() {
     return (
+        <>
+          
         <div className="footershortcut-container">
+      
             <img src="/images/logo-black.png" className="logo-black"/>
             <p className="footer-about">Simflow Plumbing provides a fully licensed and insured 24/7 plumbing service for the greater Sydney region. With over 10 years of experience within the plumbing & gas industry we offer a variety of plumbing services along with friendly and reliable service and honest up front pricing.
             We strive to deliver constant and impressive results for our clients, this is through continued training and growth of our workers. We are dedicated to continue our knowledge and skills with the often changes to the plumbing industries technology, rules
@@ -11,7 +14,7 @@ function FooterShortcut() {
 
             <h3 className="links-header">Useful Links</h3>
             <div className="useful-links">
-            <Link>Home</Link>
+            <Link >Home</Link>
             <Link>About</Link>
             <Link>Services</Link>
             <Link>Promotions</Link>
@@ -23,11 +26,11 @@ function FooterShortcut() {
             <div className="footer-contact">
             <p>Nicholas Simler | Founder</p>
        <p> <span> <i className='fa fa-phone'/> </span> 0406 300 310</p>
-            <button>Book Now</button>
+            <button className="book-now">Book Now</button>
             </div>
 
             <div className="socials">
-            <h3>Social Media</h3>
+            <h3 className="">Social Media</h3>
             <a 
                     className='socials-link'
                     href='https://www.facebook.com/Simflowplumbing0'
@@ -38,8 +41,9 @@ function FooterShortcut() {
                     <i className='fa fa-facebook-square'/>
                 </a>
             </div>
-
+         
         </div>
+        </>
     )
 }
 
