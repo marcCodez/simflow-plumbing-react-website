cf5351a4b4e20f84dae69a630622f42480209c22
Marc Pulumbarit <lastcodebender@Marcs-MacBook-Air.local>
2021-04-14 15:20:59 +1000

    about page progress

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 function About() {
     return (
-        <div>
+        <section id="about" className="text-center pb-3">
+            <div className="container">
+                <div className="about-info">
+                    <div className="heading">
+                        <h1>About Us</h1>
+                        <div className="underline"></div>
+                    </div>
+                    <p>Simflow Plumbing provides a fully licensed and insured 24/7 plumbing service for the Greater Sydney region. We offer a variety of indoor and outdoor home plumbing services evolving around maintenance, renovations, house extensions and more! 
+
+We have over 10 years of experience in the plumbing & gas industry. We guarantee a friendly and reliable service and are always sure to consider the best interest of you, the customer, in our honest up front pricing.
+
+At Simflow, we are always striving to  learn and improve through continued training and growth of our workers. We are dedicated to the continuous development of our people skills and plumbing knowledge to ensure we make our mark in the ever changing and developing plumbing industry.</p>
+                <img src="/images/work/p7.jpg" />
+                </div>
+            </div>
             
-        </div>
+        </section>
     )
 }
 
