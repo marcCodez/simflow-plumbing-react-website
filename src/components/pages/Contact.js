c55de28d3a9af136e0a427a5353807d1c548bba4
marcpulz <marc_pulumbarit@hotmail.com>
2021-06-24 00:30:36 +1000

    netlify test pt 4

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -18,20 +18,15 @@ function Contact() {
                                     </div>
                                 </Col>
                             </Row>
-                   
-                                        <form 
-                                           className="p-3"
-                    name='contact' 
-                    method='post' 
-                   netlify 
-                    onSubmit='submit'
-                    action='/success'
-                    data-netlify-honeypot='bot-field'
-                    >
-                                  <input type='hidden' name='form-name' value='contact' />
-                                    <div hidden>
-                            <input name="bot-field"/>
-                        </div>
+                            <Form 
+                                className="p-3"
+                                name="contact" 
+                                method="post" 
+                                data-netlify="true" 
+                                onSubmit="submit"
+                                >
+                                    <Form.Control type="hidden" name="form-name" value="contact"/>
+               
                             <p className="text-center font-weight-bold">Alternatively call Nicholas on 0406 300 310</p>
                             <Form.Row className="my-5">
                                 <Col lg={6}>
@@ -62,13 +57,13 @@ function Contact() {
 
                                 <Form.Row className="pt-4">
                                     <Col lg={12} className="d-flex justify-content-end">
-                                    <div data-netlify-recaptcha='true'></div>
+                            
                                     <Button className="form-btn" type="submit">
                                          Submit
                                      </Button>
                                     </Col>
                                 </Form.Row>
-                            </form>
+                            </Form>
                             
                         </Card.Body>
                         </Card>
