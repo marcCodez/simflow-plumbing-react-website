faa22da38a238fee5e168a91ebc94506ce678d46
marcpulz <marc_pulumbarit@hotmail.com>
2021-06-24 22:53:01 +1000

    center succ msg mobiles

--- src/components/pages/Success.js
+++ src/components/pages/Success.js
@@ -5,8 +5,8 @@ function Success() {
     return (
         <section id="success" className="my-5 py-5">
             <Container className="d-flex flex-column align-items-center">
-                          <i class="fas fa-check-circle tick fa-4x pb-2"></i> 
-           <p className="succ-msg">Form Successfully Submitted!</p> 
+                          <i class="fas fa-check-circle tick fa-4x mb-3"></i> 
+           <p className="succ-msg text-center">Form successfully submitted!</p> 
            </Container>
         </section>
     )
